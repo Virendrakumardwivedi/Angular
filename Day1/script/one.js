d26354781de00c0860cae4fe233288b2d1e4909f
Virendra kumar dwivedi <101566111+Virendrakumardwivedi@users.noreply.github.com>
2023-08-19 23:15:27 +0530

    Add files via upload

---
+++ Day1/script/one.js
@@ -0,0 +1,4 @@
+"use strict";
+// alert("welcome new life...!")
+let message = "welcome new life...!";
+alert(message);
---
+++ Day1/src/one.ts
@@ -0,0 +1,4 @@
+// alert("welcome new life...!")
+
+let message:String ="welcome new life...!";
+alert(message)
